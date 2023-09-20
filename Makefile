412cd3d28b3ade3870ebc0d92ea09c1b2885279e
Robotechnic <robotechnic.developeur@gmail.com>
2023-09-20 09:38:09 +0200

    change rust code to c code

--- Makefile
+++ Makefile
@@ -1,9 +1,13 @@
 wasm :
-	cd graphviz; \
-	cargo build --target wasm32-wasi --release
-	cp graphviz/target/wasm32-wasi/release/graphviz.wasm ./graphviz.wasm
+	cd graphviz_interface; \
+	make -j16
+
+link :
+	mkdir -p ~/.cache/typst/packages/preview/typst-graphviz
+	ln -s "$(CURDIR)" ~/.cache/typst/packages/preview/typst-graphviz/0.1.0
 
 clean :
-	cd graphviz; \
+	cd graphviz_interface; \
 	cargo clean
 	rm -f graphviz.wasm
+	rm -rf ~/.cache/typst/packages/preview/graphviz
---
+++ graphviz_interface/main.c
@@ -0,0 +1,57 @@
+#include "emscripten.h"
+
+#include <string.h>
+#include <graphviz/gvc.h>
+
+
+#define PROTOCOL_FUNCTION __attribute__((import_module("typst_env"))) extern
+
+PROTOCOL_FUNCTION void wasm_minimal_protocol_send_result_to_host(const uint8_t *ptr, size_t len);
+PROTOCOL_FUNCTION void wasm_minimal_protocol_write_args_to_buffer(uint8_t *ptr);
+
+EMSCRIPTEN_KEEPALIVE
+int render(size_t arg1_len) {
+    uint8_t *arg1 = malloc(arg1_len);
+    if (!arg1) {
+        return 1;
+    }
+    wasm_minimal_protocol_write_args_to_buffer(arg1);
+    
+    char errbuf[4096];
+    GVC_t *gvc = gvContext();
+    graph_t *g = agmemread((char *)arg1);
+    if (!g) {
+        char *err = malloc(strlen(errbuf) + 7);
+        err = strcat(err, "error: ");
+        err = strcat(err, errbuf);
+        wasm_minimal_protocol_send_result_to_host((uint8_t *)err, strlen(err));
+        return 1;
+    }
+
+    if (gvLayout(gvc, g, "dot")) {
+        char *err = malloc(strlen(errbuf) + 7);
+        err = strcat(err, "error: ");
+        err = strcat(err, errbuf);
+		return 1;
+	}
+
+    char* data;
+    unsigned int length;
+
+    if (gvRenderData(gvc, g, "svg", &data, &length)) {
+        gvFreeRenderData(data);
+        return 1;
+    }
+    if (g) {
+	    gvFreeLayout(gvc, g);
+    }
+	gvFinalize(gvc);
+    gvFreeContext(gvc);
+    agclose(g);
+
+
+    // wasm_minimal_protocol_send_result_to_host((uint8_t *)result, length);
+    wasm_minimal_protocol_send_result_to_host(arg1, arg1_len);
+    free(arg1);
+    return 0;
+}
