6d0807d582f153283e087d5ee64aab05cfe286d6
Robotechnic <robotechnic.developeur@gmail.com>
2023-09-21 17:47:29 +0200

    fix some bugs in the render function, add exemples (it doesn't works all the time for now)

--- graphviz_interface/main.c
+++ graphviz_interface/main.c
@@ -21,7 +21,7 @@ lt_symlist_t lt_preloaded_symbols[] = {
     {0, 0}
 };
 
-char errBuff[4096];
+char errBuff[1024];
 int vizErrorf(char *str) {
     strncpy(errBuff + 7, str, sizeof(errBuff) - 7);
     errBuff[0] = 'e';
@@ -42,49 +42,56 @@ int vizErrorf(char *str) {
  */
 EMSCRIPTEN_KEEPALIVE
 int render(size_t dot_len, size_t engine_len) {
-    errBuff[0] = '\0';
+    // set error report to custom function
+    // it allow us to get the error message from graphviz
     agseterr(AGERR);
     agseterrf(vizErrorf);
 
-    uint8_t *buffer = malloc(dot_len + engine_len);
+    uint8_t *buffer = malloc(dot_len + engine_len + 1);
+
     if (!buffer) {
         return 1;
     }
     wasm_minimal_protocol_write_args_to_buffer(buffer);
-    char* dot = (char *)buffer;
-    char* engine = (char *)buffer + dot_len;
+    // to make sure the buffer is null terminated
+    buffer[dot_len + engine_len] = '\0';
+
+    char const* dot = (char *)buffer;
+    char const* engine = (char *)buffer + dot_len;
 
     GVC_t *gvc = gvContextPlugins(lt_preloaded_symbols, true);
-    graph_t *g = agmemread((char *)dot);
+    graph_t *g = agmemread(dot);
+    char* data = NULL;
+    unsigned int length;
+
     if (!g) {
-        free(buffer);
         wasm_minimal_protocol_send_result_to_host((uint8_t *)errBuff, strlen(errBuff));
+        free(buffer);
         return 0;
     }
 
     if (gvLayout(gvc, g, engine) == -1) {
-        free(buffer);
         wasm_minimal_protocol_send_result_to_host((uint8_t *)errBuff, strlen(errBuff));
+        free(buffer);
 		return 0;
 	}
 
-    char* data;
-    unsigned int length;
     int result = gvRenderData(gvc, g, "svg", &data, &length);
-    free(buffer);
     if (result == -1) {
         gvFreeRenderData(data);
+        free(buffer);
+        char* err = "error: failed to render graph to svg\0";
+        wasm_minimal_protocol_send_result_to_host((uint8_t *)err, strlen(err));
         return 1;
     }
-    if (g) {
-	    gvFreeLayout(gvc, g);
-    }
-	gvFinalize(gvc);
-    gvFreeContext(gvc);
-    agclose(g);
-
 
     wasm_minimal_protocol_send_result_to_host((uint8_t *)data, length);
+    gvFreeRenderData(data);
+    gvFreeLayout(gvc, g);
+    agclose(g);
+	gvFinalize(gvc);
+    gvFreeContext(gvc);
+    free(buffer);
     return 0;
 }
 
--- lib.typ
+++ lib.typ
@@ -1,19 +1,33 @@
 #let plugin = plugin("graphviz.wasm")
 
-#let render(text, width: auto, height: auto, fit: "cover") = {
-	let render = str(plugin.render(bytes(text)))
-	return raw(render)
-	// if render.slice(0, 6) == "error:" {
-	// 	return raw(render)
-	// } else {
-	// 	return image.decode(
-	// 		render,
-	// 		"svg"
-	// 	)
-	// }
+#let render(text, engine: "dot", width: auto, height: auto, fit: "cover") = {
+	if text.len() != bytes(text).len() {
+		return raw("error: text must be utf-8 encoded")
+	}
+
+	// add null terminator
+	let encodedText = bytes(text) + bytes((0,))
+	let encodedEngine = bytes(engine) + bytes((0,))
+
+	let render = str(
+		plugin.render(encodedText, encodedEngine)
+	)
+
+	if render.slice(0, 6) == "error:" {
+		return raw(render)
+	} else {
+		return image.decode(
+			render,
+			format: "svg",
+			height: height,
+			width: width,
+			fit: fit,
+		)
+	}
 }
 
-// #show raw.where(lang: "dotrender"): it => {
-// 	let text = it.text()
-	
-// }
+#show raw.where(lang: "dotrender"): it => {
+	let text = it.text()
+	return render(text)
+}
+
