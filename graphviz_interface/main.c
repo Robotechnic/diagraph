4b1f81ed13543dece65cd11d8ea481eccf0cbcf3
Robotechnic <robotechnic.developeur@gmail.com>
2023-11-02 19:03:06 +0100

    now the c code send a different output

--- graphviz_interface/main.c
+++ graphviz_interface/main.c
@@ -2,6 +2,7 @@
 
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 #include <graphviz/gvc.h>
 #include <graphviz/gvplugin.h>
 
@@ -24,15 +25,22 @@ lt_symlist_t lt_preloaded_symbols[] = {
 
 char errBuff[1024];
 int vizErrorf(char *str) {
-    strncpy(errBuff + 7, str, sizeof(errBuff) - 7);
-    errBuff[0] = 'e';
-    errBuff[1] = 'r';
+    strncpy(errBuff + 7, str, sizeof(errBuff) - 8);
+    errBuff[0] = 1;
+    errBuff[1] = 'e';
     errBuff[2] = 'r';
-    errBuff[3] = 'o';
-    errBuff[4] = 'r';
-    errBuff[5] = ':';
-    errBuff[6] = ' ';
+    errBuff[3] = 'r';
+    errBuff[4] = 'o';
+    errBuff[5] = 'r';
+    errBuff[6] = ':';
+    errBuff[7] = ' ';
     return 0;
+}
+
+void big_endian_encode(uint8_t *buffer, int value) {
+    for (int i = 0; i < sizeof(int); i++) {
+        buffer[i] = (value >> (8 * (sizeof(int) - i - 1))) & 0xFF;
+    }
 }
 
 /**
@@ -51,7 +59,7 @@ int render(size_t dot_len, size_t engine_len, size_t background_len) {
     uint8_t *buffer = malloc(dot_len + engine_len + background_len);
 
     if (!buffer) {
-        return 1;
+      return 1;
     }
     wasm_minimal_protocol_write_args_to_buffer(buffer);
 
@@ -59,8 +67,8 @@ int render(size_t dot_len, size_t engine_len, size_t background_len) {
     // is not null terminated and because
     // the buffer doesn't reset properly when
     // watch mode is enabled
-    char* dot = malloc(dot_len + 1);
-    char* engine = malloc(engine_len + 1);
+    char *dot = malloc(dot_len + 1);
+    char *engine = malloc(engine_len + 1);
     char *background = malloc(background_len + 1);
     memcpy(dot, buffer, dot_len);
     dot[dot_len] = '\0';
@@ -70,46 +78,62 @@ int render(size_t dot_len, size_t engine_len, size_t background_len) {
     background[background_len] = '\0';
     free(buffer);
 
-    GVC_t *gvc = gvContextPlugins(lt_preloaded_symbols, true);
+    GVC_t *gvc = gvContext();
     graph_t *g = agmemread(dot);
 
     if (strlen(background) > 0) {
-        agattr(g, AGRAPH, "bgcolor", background);
+      agattr(g, AGRAPH, "bgcolor", background);
     }
 
     agattr(g, AGRAPH, "center", "true");
+    agattr(g, AGRAPH, "margin", "0");
 
     free(background);
     free(dot);
-    char* data = NULL;
+    char *data = NULL;
     unsigned int length;
 
     if (!g) {
-        free(engine);
-        wasm_minimal_protocol_send_result_to_host((uint8_t *)errBuff, strlen(errBuff));
-        return 0;
+      free(engine);
+      wasm_minimal_protocol_send_result_to_host((uint8_t *)errBuff,
+                                                strlen(errBuff));
+      return 0;
     }
 
     if (gvLayout(gvc, g, engine) == -1) {
-        free(engine);
-        wasm_minimal_protocol_send_result_to_host((uint8_t *)errBuff, strlen(errBuff));
-		return 0;
-	}
+      free(engine);
+      wasm_minimal_protocol_send_result_to_host((uint8_t *)errBuff,
+                                                strlen(errBuff));
+      return 0;
+    }
 
     int result = gvRenderData(gvc, g, "svg", &data, &length);
     free(engine);
     if (result == -1) {
-        gvFreeRenderData(data);
-        char* err = "error: failed to render graph to svg\0";
-        wasm_minimal_protocol_send_result_to_host((uint8_t *)err, strlen(err));
-        return 1;
+      gvFreeRenderData(data);
+      char *err = "error: failed to render graph to svg\0";
+      wasm_minimal_protocol_send_result_to_host((uint8_t *)err, strlen(err));
+      return 1;
     }
 
-    wasm_minimal_protocol_send_result_to_host((uint8_t *)data, length);
+    // display bounding box of the svg render in the console
+    // this is useful to debug the svg render
+    int width = floor(GD_bb(g).UR.x - GD_bb(g).LL.x);
+    int height = floor(GD_bb(g).UR.y - GD_bb(g).LL.y);
+
+    size_t result_buffer_len = 0;
+    result_buffer_len = sizeof(width) + sizeof(height) + length + 1;
+    uint8_t *result_buffer = malloc(result_buffer_len);
+    result_buffer[0] = 0;
+    big_endian_encode(result_buffer + 1, width);
+    big_endian_encode(result_buffer + 1 + sizeof(width), height);
+    memcpy(result_buffer + 1 + sizeof(width) + sizeof(height), data, length);
+
+    wasm_minimal_protocol_send_result_to_host(result_buffer, result_buffer_len);
     gvFreeRenderData(data);
     gvFreeLayout(gvc, g);
     agclose(g);
-	gvFinalize(gvc);
+    gvFinalize(gvc);
     gvFreeContext(gvc);
     return 0;
 }
