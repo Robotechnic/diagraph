bf2e93a34901dc6d07f9d031f3e0613623989472
Robotechnic <robotechnic.developeur@gmail.com>
2024-03-25 13:09:01 +0100

    move graphviz related code to a separated header

--- graphviz_interface/src/protocol/protocol.h
+++ graphviz_interface/src/protocol/protocol.h
@@ -2,25 +2,12 @@
 #define PROTOCOL_H
 
 #include "emscripten.h"
-#include <graphviz/gvplugin.h>
 #include <stdint.h>
 
 #define PROTOCOL_FUNCTION __attribute__((import_module("typst_env"))) extern
 
 PROTOCOL_FUNCTION void wasm_minimal_protocol_send_result_to_host(const uint8_t *ptr, size_t len);
 PROTOCOL_FUNCTION void wasm_minimal_protocol_write_args_to_buffer(uint8_t *ptr);
-
-extern gvplugin_library_t gvplugin_dot_layout_LTX_library;
-extern gvplugin_library_t gvplugin_neato_layout_LTX_library;
-
-extern gvplugin_library_t gvplugin_core_LTX_library;
-
-lt_symlist_t lt_preloaded_symbols[] = {
-    {"gvplugin_dot_layout_LTX_library", &gvplugin_dot_layout_LTX_library},
-    {"gvplugin_neato_layout_LTX_library", &gvplugin_neato_layout_LTX_library},
-    {"gvplugin_core_LTX_library", &gvplugin_core_LTX_library},
-    {0, 0}};
-
 
 #define INIT_BUFFER_UNPACK(buffer_len)                                                             \
     size_t __buffer_offset = 0;                                                                    \
