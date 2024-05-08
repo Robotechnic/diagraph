ef6a0b524da0b2abb003167da31bfb34705cd6be
Robotechnic <robotechnic.developeur@gmail.com>
2024-05-08 19:49:50 +0200

    changes how the protocol works to use less data and be much easier to manipulate

--- graphviz_interface/src/protocol/protocol.c
+++ graphviz_interface/src/protocol/protocol.c
@@ -68,39 +68,40 @@ int decode_SizedLabel(uint8_t *__input_buffer, size_t buffer_len, SizedLabel *ou
     int err;
     (void)err;
     NEXT_STR(out->label)
+    NEXT_CHAR(out->native)
     NEXT_FLOAT(out->width)
     NEXT_FLOAT(out->height)
     *buffer_offset += __buffer_offset;
     return 0;
 }
-void free_NativeLabel(NativeLabel *s) {
-    if (s->label) {
-        free(s->label);
-    }
+void free_Coordinates(Coordinates *s) {
 }
-size_t NativeLabel_size(const void *s){
-	return strlen(((NativeLabel*)s)->label) + 1 + 1;
+size_t Coordinates_size(const void *s){
+	return TYPST_INT_SIZE + TYPST_INT_SIZE;
 }
-int encode_NativeLabel(const NativeLabel *s, uint8_t *__input_buffer, size_t *buffer_len, size_t *buffer_offset) {
-    size_t __buffer_offset = 0;    size_t s_size = NativeLabel_size(s);
+int encode_Coordinates(const Coordinates *s, uint8_t *__input_buffer, size_t *buffer_len, size_t *buffer_offset) {
+    size_t __buffer_offset = 0;    size_t s_size = Coordinates_size(s);
     if (s_size > *buffer_len) {
         return 2;
     }
     int err;
 	(void)err;
-    STR_PACK(s->label)
-    CHAR_PACK(s->mathMode)
+    FLOAT_PACK(s->x)
+    FLOAT_PACK(s->y)
 
     *buffer_offset += __buffer_offset;
     return 0;
 }
 void free_LabelInfo(LabelInfo *s) {
+    if (s->label) {
+        free(s->label);
+    }
     if (s->fontName) {
         free(s->fontName);
     }
 }
 size_t LabelInfo_size(const void *s){
-	return TYPST_INT_SIZE + TYPST_INT_SIZE + TYPST_INT_SIZE + strlen(((LabelInfo*)s)->fontName) + 1 + TYPST_INT_SIZE;
+	return 1 + strlen(((LabelInfo*)s)->label) + 1 + 1 + TYPST_INT_SIZE + strlen(((LabelInfo*)s)->fontName) + 1 + TYPST_INT_SIZE;
 }
 int encode_LabelInfo(const LabelInfo *s, uint8_t *__input_buffer, size_t *buffer_len, size_t *buffer_offset) {
     size_t __buffer_offset = 0;    size_t s_size = LabelInfo_size(s);
@@ -109,13 +110,48 @@ int encode_LabelInfo(const LabelInfo *s, uint8_t *__input_buffer, size_t *buffer
     }
     int err;
 	(void)err;
-    FLOAT_PACK(s->x)
-    FLOAT_PACK(s->y)
+    CHAR_PACK(s->native)
+    STR_PACK(s->label)
+    CHAR_PACK(s->mathMode)
     INT_PACK(s->color)
     STR_PACK(s->fontName)
     FLOAT_PACK(s->fontSize)
 
     *buffer_offset += __buffer_offset;
+    return 0;
+}
+void free_renderGraph(renderGraph *s) {
+    if (s->dot) {
+        free(s->dot);
+    }
+    for (size_t i = 0; i < s->labels_len; i++) {
+    free_SizedLabel(&s->labels[i]);
+    }
+    free(s->labels);
+    if (s->engine) {
+        free(s->engine);
+    }
+}
+int decode_renderGraph(size_t buffer_len, renderGraph *out) {
+    INIT_BUFFER_UNPACK(buffer_len)
+    int err;
+    (void)err;
+    NEXT_FLOAT(out->fontSize)
+    NEXT_STR(out->dot)
+    NEXT_INT(out->labels_len)
+    if (out->labels_len == 0) {
+        out->labels = NULL;
+    } else {
+        out->labels = malloc(out->labels_len * sizeof(SizedLabel));
+        if (!out->labels){
+            return 1;
+        }
+        for (size_t i = 0; i < out->labels_len; i++) {
+    if ((err = decode_SizedLabel(__input_buffer + __buffer_offset, buffer_len - __buffer_offset, &out->labels[i], &__buffer_offset))){return err;}
+        }
+    }
+    NEXT_STR(out->engine)
+    FREE_BUFFER()
     return 0;
 }
 void free_overriddenLabels(overriddenLabels *s) {
@@ -149,71 +185,41 @@ int decode_overriddenLabels(size_t buffer_len, overriddenLabels *out) {
     FREE_BUFFER()
     return 0;
 }
-void free_renderGraph(renderGraph *s) {
-    if (s->dot) {
-        free(s->dot);
+void free_LabelsInfos(LabelsInfos *s) {
+    for (size_t i = 0; i < s->labels_len; i++) {
+    free_LabelInfo(&s->labels[i]);
     }
-    for (size_t i = 0; i < s->nativeLabels_len; i++) {
-    free_SizedLabel(&s->nativeLabels[i]);
-    }
-    free(s->nativeLabels);
-    for (size_t i = 0; i < s->manualLabels_len; i++) {
-    free_SizedLabel(&s->manualLabels[i]);
-    }
-    free(s->manualLabels);
-    if (s->engine) {
-        free(s->engine);
-    }
+    free(s->labels);
 }
-int decode_renderGraph(size_t buffer_len, renderGraph *out) {
-    INIT_BUFFER_UNPACK(buffer_len)
+size_t LabelsInfos_size(const void *s){
+	return TYPST_INT_SIZE + list_size(((LabelsInfos*)s)->labels, ((LabelsInfos*)s)->labels_len, LabelInfo_size, sizeof(*((LabelsInfos*)s)->labels));
+}
+int encode_LabelsInfos(const LabelsInfos *s) {
+    size_t buffer_len = LabelsInfos_size(s);
+    INIT_BUFFER_PACK(buffer_len)
     int err;
-    (void)err;
-    NEXT_FLOAT(out->fontSize)
-    NEXT_STR(out->dot)
-    NEXT_INT(out->nativeLabels_len)
-    if (out->nativeLabels_len == 0) {
-        out->nativeLabels = NULL;
-    } else {
-        out->nativeLabels = malloc(out->nativeLabels_len * sizeof(SizedLabel));
-        if (!out->nativeLabels){
-            return 1;
-        }
-        for (size_t i = 0; i < out->nativeLabels_len; i++) {
-    if ((err = decode_SizedLabel(__input_buffer + __buffer_offset, buffer_len - __buffer_offset, &out->nativeLabels[i], &__buffer_offset))){return err;}
+	(void)err;
+    INT_PACK(s->labels_len)
+    for (size_t i = 0; i < s->labels_len; i++) {
+        if ((err = encode_LabelInfo(&s->labels[i], __input_buffer + __buffer_offset, &buffer_len, &__buffer_offset))) {
+            return err;
         }
     }
-    NEXT_INT(out->manualLabels_len)
-    if (out->manualLabels_len == 0) {
-        out->manualLabels = NULL;
-    } else {
-        out->manualLabels = malloc(out->manualLabels_len * sizeof(SizedLabel));
-        if (!out->manualLabels){
-            return 1;
-        }
-        for (size_t i = 0; i < out->manualLabels_len; i++) {
-    if ((err = decode_SizedLabel(__input_buffer + __buffer_offset, buffer_len - __buffer_offset, &out->manualLabels[i], &__buffer_offset))){return err;}
-        }
-    }
-    NEXT_STR(out->engine)
-    FREE_BUFFER()
+
+    wasm_minimal_protocol_send_result_to_host(__input_buffer, buffer_len);
     return 0;
 }
 void free_graphInfo(graphInfo *s) {
-    for (size_t i = 0; i < s->manualLabels_len; i++) {
-    free_LabelInfo(&s->manualLabels[i]);
+    for (size_t i = 0; i < s->labels_len; i++) {
+    free_Coordinates(&s->labels[i]);
     }
-    free(s->manualLabels);
-    for (size_t i = 0; i < s->nativeLabels_len; i++) {
-    free_LabelInfo(&s->nativeLabels[i]);
-    }
-    free(s->nativeLabels);
+    free(s->labels);
     if (s->svg) {
         free(s->svg);
     }
 }
 size_t graphInfo_size(const void *s){
-	return 1 + TYPST_INT_SIZE + list_size(((graphInfo*)s)->manualLabels, ((graphInfo*)s)->manualLabels_len, LabelInfo_size, sizeof(*((graphInfo*)s)->manualLabels)) + TYPST_INT_SIZE + list_size(((graphInfo*)s)->nativeLabels, ((graphInfo*)s)->nativeLabels_len, LabelInfo_size, sizeof(*((graphInfo*)s)->nativeLabels)) + strlen(((graphInfo*)s)->svg) + 1;
+	return 1 + TYPST_INT_SIZE + list_size(((graphInfo*)s)->labels, ((graphInfo*)s)->labels_len, Coordinates_size, sizeof(*((graphInfo*)s)->labels)) + strlen(((graphInfo*)s)->svg) + 1;
 }
 int encode_graphInfo(const graphInfo *s) {
     size_t buffer_len = graphInfo_size(s);
@@ -221,43 +227,13 @@ int encode_graphInfo(const graphInfo *s) {
     int err;
 	(void)err;
     CHAR_PACK(s->error)
-    INT_PACK(s->manualLabels_len)
-    for (size_t i = 0; i < s->manualLabels_len; i++) {
-        if ((err = encode_LabelInfo(&s->manualLabels[i], __input_buffer + __buffer_offset, &buffer_len, &__buffer_offset))) {
-            return err;
-        }
-    }
-    INT_PACK(s->nativeLabels_len)
-    for (size_t i = 0; i < s->nativeLabels_len; i++) {
-        if ((err = encode_LabelInfo(&s->nativeLabels[i], __input_buffer + __buffer_offset, &buffer_len, &__buffer_offset))) {
+    INT_PACK(s->labels_len)
+    for (size_t i = 0; i < s->labels_len; i++) {
+        if ((err = encode_Coordinates(&s->labels[i], __input_buffer + __buffer_offset, &buffer_len, &__buffer_offset))) {
             return err;
         }
     }
     STR_PACK(s->svg)
-
-    wasm_minimal_protocol_send_result_to_host(__input_buffer, buffer_len);
-    return 0;
-}
-void free_nativeLabels(nativeLabels *s) {
-    for (size_t i = 0; i < s->nativeLabels_len; i++) {
-    free_NativeLabel(&s->nativeLabels[i]);
-    }
-    free(s->nativeLabels);
-}
-size_t nativeLabels_size(const void *s){
-	return TYPST_INT_SIZE + list_size(((nativeLabels*)s)->nativeLabels, ((nativeLabels*)s)->nativeLabels_len, NativeLabel_size, sizeof(*((nativeLabels*)s)->nativeLabels));
-}
-int encode_nativeLabels(const nativeLabels *s) {
-    size_t buffer_len = nativeLabels_size(s);
-    INIT_BUFFER_PACK(buffer_len)
-    int err;
-	(void)err;
-    INT_PACK(s->nativeLabels_len)
-    for (size_t i = 0; i < s->nativeLabels_len; i++) {
-        if ((err = encode_NativeLabel(&s->nativeLabels[i], __input_buffer + __buffer_offset, &buffer_len, &__buffer_offset))) {
-            return err;
-        }
-    }
 
     wasm_minimal_protocol_send_result_to_host(__input_buffer, buffer_len);
     return 0;
--- graphviz_interface/src/protocol/protocol.h
+++ graphviz_interface/src/protocol/protocol.h
@@ -108,25 +108,37 @@ PROTOCOL_FUNCTION void wasm_minimal_protocol_write_args_to_buffer(uint8_t *ptr);
     }
 typedef struct {
     char* label;
+    bool native;
     float width;
     float height;
 } SizedLabel;
 void free_SizedLabel(SizedLabel *s);
 
 typedef struct {
-    char* label;
-    bool mathMode;
-} NativeLabel;
-void free_NativeLabel(NativeLabel *s);
-
-typedef struct {
     float x;
     float y;
+} Coordinates;
+void free_Coordinates(Coordinates *s);
+
+typedef struct {
+    bool native;
+    char* label;
+    bool mathMode;
     int color;
     char* fontName;
     float fontSize;
 } LabelInfo;
 void free_LabelInfo(LabelInfo *s);
+
+typedef struct {
+    float fontSize;
+    char* dot;
+    SizedLabel * labels;
+    size_t labels_len;
+    char* engine;
+} renderGraph;
+void free_renderGraph(renderGraph *s);
+int decode_renderGraph(size_t buffer_len, renderGraph *out);
 
 typedef struct {
     char* * labels;
@@ -137,33 +149,19 @@ void free_overriddenLabels(overriddenLabels *s);
 int decode_overriddenLabels(size_t buffer_len, overriddenLabels *out);
 
 typedef struct {
-    float fontSize;
-    char* dot;
-    SizedLabel * nativeLabels;
-    size_t nativeLabels_len;
-    SizedLabel * manualLabels;
-    size_t manualLabels_len;
-    char* engine;
-} renderGraph;
-void free_renderGraph(renderGraph *s);
-int decode_renderGraph(size_t buffer_len, renderGraph *out);
+    LabelInfo * labels;
+    size_t labels_len;
+} LabelsInfos;
+void free_LabelsInfos(LabelsInfos *s);
+int encode_LabelsInfos(const LabelsInfos *s);
 
 typedef struct {
     bool error;
-    LabelInfo * manualLabels;
-    size_t manualLabels_len;
-    LabelInfo * nativeLabels;
-    size_t nativeLabels_len;
+    Coordinates * labels;
+    size_t labels_len;
     char* svg;
 } graphInfo;
 void free_graphInfo(graphInfo *s);
 int encode_graphInfo(const graphInfo *s);
-
-typedef struct {
-    NativeLabel * nativeLabels;
-    size_t nativeLabels_len;
-} nativeLabels;
-void free_nativeLabels(nativeLabels *s);
-int encode_nativeLabels(const nativeLabels *s);
 
 #endif
